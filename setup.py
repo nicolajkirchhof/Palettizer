817c133d4e5724df45e48873bc1d1cb06c6c2705
nicolajkirchhof <nicolaj.kirchhof@tu-dortmund.de>
2016-02-06 00:17:57 +0100

    Initial Checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='palettizer',
+    version='0.0.1',
+    packages=['palettizer'],
+    url='',
+    license='',
+    author='nicol',
+    author_email='',
+    description=''
+)
